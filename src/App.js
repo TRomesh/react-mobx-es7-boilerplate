fa69c18d6d6ff8e22311038fe873450f56e98e0a
Yoni Weisbrod <yoni@artsetters.com>
2016-11-28 15:05:23 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { observable, computed, useStrict, action } from 'mobx';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+
 
 class App extends Component {
   render() {
